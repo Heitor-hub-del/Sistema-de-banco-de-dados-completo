2f11f615c201452243b9aef50b1a8796b57acf6e
Heitor-hub-del <heitortrader2023@gmail.com>
2026-01-14 16:51:15 -0300

    1.1 corrreção de bugs: removido o loop depois do primeiro cadastro e remoção do print no database.py

--- database.py
+++ database.py
@@ -1,11 +1,12 @@
 import pandas as pd
 import Registrer_system
+import Login_system
 
 Dados_pessoas = Registrer_system.Email_usuario
 Dados_senha = Registrer_system.Senha_usuario
 Dados_Nome_Usuario = Registrer_system.Nome_usuario
 Dados_Sobrenome_Usuario = Registrer_system.Sobrenome_usuario
 
-print(f"Bem vindo {Dados_Nome_Usuario} {Dados_Sobrenome_Usuario}, sua senha é {Dados_senha} e seu email é {Dados_pessoas}")
+
 
 
